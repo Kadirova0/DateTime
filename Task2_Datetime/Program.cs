039dfef3e88005bf0f3e3ae941b96cface22fcce
Fatimə Qədirova <fatimagg@code.edu.az>
2023-07-06 17:48:17 +0400

    DateTime

---
+++ Task2_Datetime/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace datetime2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime time1 = new DateTime(05 / 08 / 2001);
+            time1 = time1.AddDays(30);
+            time1 = time1.AddHours(6);
+            time1 = time1.AddMinutes(17);
+            Console.WriteLine(time1);
+        }
+    }
+}
---
+++ Task_Datetime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Datetimee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DateTime date = DateTime.ParseExact("05_08_2001", "dd_MM_yyyy", CultureInfo.InvariantCulture);
+                Console.WriteLine(date);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message);
+            }
+
+        }
+    }
+}
